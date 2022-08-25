3c932a9533fc8509ce26deb7a9629011e7b580e6
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 <michaeloziljoya@gmail.com>
2022-08-24 21:27:39 -0500

    unimos al header mobile la seccion del menu para mobile, y creamos @mediaquerys nesesarios para que se vea bonito.

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 //toggle permite agregar o eliminar, en este caso el inactive que creamos en css para que cuando tengamos el evento click aparezca o desaparezca.
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
